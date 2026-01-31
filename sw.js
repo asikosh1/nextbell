dd4d42935aa303c07b92ae99bcf3de322c7708fc
asikosh <as1k0n@outlook.com>
2026-01-31 22:51:41 +0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,3 +1,16 @@
+const CACHE_NAME = 'nextbell-v1';
+
+// Установка воркера
+self.addEventListener('install', (event) => {
+    self.skipWaiting();
+});
+
+// Активация
+self.addEventListener('activate', (event) => {
+    event.waitUntil(clients.claim());
+});
+
+// Обработка запросов (пустой обработчик нужен для PWA)
 self.addEventListener('fetch', (event) => {
-   
+    // Просто пропускаем запросы в сеть
 });
